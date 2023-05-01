07accbad8aaed596364eda03fc626ed1d4041729
sorwalisson <sorwalisson@gmail.com>
2023-05-01 04:19:09 -0300

    criação do model Order_item

--- app/models/product_model.rb
+++ app/models/product_model.rb
@@ -1,5 +1,7 @@
 class ProductModel < ApplicationRecord
   belongs_to :supplier
+  has_many :order_items
+  has_many :orders, through: :order_items
   validates :name, :weight, :width, :height, :depth, :sku, presence: true
   validates :sku, uniqueness: true
   validates :sku, length: {is: 20}
--- spec/system/order/user_view_own_orders_spec.rb
+++ spec/system/order/user_view_own_orders_spec.rb
@@ -77,4 +77,33 @@ describe 'usuário vê seus proprios pedidos' do
     expect(page).to have_content "Acesso Negado, você não é o usuário que gerou esse pedido"
   end
 
+  it 'e vê items do pedido' do
+    #arrange
+    new_user = User.create!(name: "Walisson", email: "sorwalisson@email.com", password: "password")
+    supplier = Supplier.create!(corporate_name: 'Samsung korea LTDA', brand_name: 'SAMSUNG', registration_number: "123456789000", 
+                                full_address: "Avenida itapuã, 35", city: "São Paulo", state: "SP", email: "samsung@sac.com.br")
+    product_a = ProductModel.create!(name: "TV 32", weight: 8000, width: 70, height: 45, depth: 10, 
+                                     sku: "TV32PL-SAMSUG-XPT090", supplier_id: supplier.id)
+    product_b = ProductModel.create!(name: "Sound System 7.1", weight: 2800, width: 60, height: 50, depth: 30, 
+                                     sku: 'SOSAM1-SAMSU2-LOUD77', supplier_id: supplier.id)
+    product_c = ProductModel.create!(name: "Samsung Galaxy A30", weight: 1200, width: 30, height: 20, depth: 15, 
+                                    sku: 'SAGGLY-SAMPH2-MPHM77', supplier_id: supplier.id)
+    warehouse = Warehouse.create!(name: "Aeroporto SP", code: "GRU", city: "Guarulhos", area: 100_000, address: 'Avenida do Aeroporto, 1000', 
+                                  cep: '15000-000', description: 'Galpão destinado a encomendas internacionais.')
+    date = 1.day.from_now                             
+    first_order = Order.create!(user_id: new_user.id, warehouse_id: warehouse.id, supplier_id: supplier.id, estimated_delivery_date: date.strftime("%d/%m/%Y"))
+    OrderItem.create!(product_model_id: product_a.id, order_id: first_order.id, quantity: 20)
+    OrderItem.create!(product_model_id: product_b.id, order_id: first_order.id, quantity: 12)
+    
+    #act
+    login_as(new_user)
+    visit root_path
+    click_on "Meus Pedidos"
+    click_on first_order.code
+
+    #assert
+    expect(page).to have_content 'Itens do Pedido'
+    expect(page).to have_content '20 x TV 32'
+    expect(page).to have_content '12 x Sound System 7.1'
+  end
 end
